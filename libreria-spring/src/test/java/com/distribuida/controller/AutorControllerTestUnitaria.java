53be25c73a01c40de6100c5414e8d6e587d4d0b5
Cristian Barreno <cristianbarrenos@gmail.com>
2025-09-13 20:39:47 -0500

    carrito compras

--- libreria-spring/src/test/java/com/distribuida/controller/AutorControllerTestUnitaria.java
+++ libreria-spring/src/test/java/com/distribuida/controller/AutorControllerTestUnitaria.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-public class AutorControllerTestUnitaria {
+public class    AutorControllerTestUnitaria {
 
     @InjectMocks
     private AutorController autorController;
